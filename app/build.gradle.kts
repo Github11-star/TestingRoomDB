ca62b83cd289adbd4693c969c218c1ef3eb65c99
Firoz Shaikh <gitfiroz695@gmail.com>
2023-08-30 13:52:15 +0530

    Room database test case for quoteDao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -46,7 +46,7 @@ dependencies {
     // Room database dependencies
     val room_version = "2.5.2"
     implementation("androidx.room:room-runtime:$room_version")
-    kapt ("androidx.room:room-compiler:$room_version")
+    ksp ("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
---
+++ app/src/main/java/com/firozpocyt/testingroomdb/Quote.kt
@@ -0,0 +1,13 @@
+package com.firozpocyt.testingroomdb
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Quote(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val text: String,
+    val author: String
+)
